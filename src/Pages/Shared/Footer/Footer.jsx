146701185066403992f0339f17ea5224910ff9c3
NishatFariza <nishatfarzia78@gmail.com>
2022-06-01 00:13:24 +0600

    footer added

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import "./Footer.css";
 import { FaEnvelope } from "react-icons/fa";
+import { BsTelephoneFill } from "react-icons/bs";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa";
+import { BsWhatsapp } from "react-icons/bs";
+import { FiInstagram } from "react-icons/fi";
 
 const Footer = () => {
   const today = new Date();
@@ -9,19 +14,34 @@ const Footer = () => {
     <div className="footer-bg ">
       <div className="py-14 px-10 grid grid-cols-3 gap-5">
         <div className="flex  justify-start items-center">
-          <FaEnvelope className="text-white mr-4"></FaEnvelope>
-          <p className="text-stone-400">info@gmail.com</p>
-          <p className="text-stone-400">info@gmail.com</p>
-        </div>
-        <div className="flex justify-center items-center ">
-          <FaEnvelope className="text-white mr-4"></FaEnvelope>
-          <p className="text-stone-400">info@gmail.com</p>
+          <div className="w-[40px] h-[40px] rounded-full bg-blue-600 text-white flex justify-center items-center ">
+            <FaEnvelope className="text-sm cursor-pointer"></FaEnvelope>
+          </div>
+          <p className="text-stone-400 ml-2">info@gmail.com</p>
+          <p className="text-stone-400 mx-2">/</p>
           <p className="text-stone-400">info@gmail.com</p>
         </div>
         <div className="flex justify-center items-center ">
-          <FaEnvelope className="text-white"></FaEnvelope>
-          <FaEnvelope className="text-white mx-4"></FaEnvelope>
-          <FaEnvelope className="text-white"></FaEnvelope>
+          <div className="w-[40px] h-[40px] rounded-full bg-blue-600 text-white flex justify-center items-center ">
+            <BsTelephoneFill className="text-sm cursor-pointer"></BsTelephoneFill>
+          </div>
+          <p className="text-stone-400  ml-2">+9 000000000</p>
+          <p className="text-stone-400 mx-2">/</p>
+          <div className="w-[40px] h-[40px] rounded-full bg-blue-600 text-white flex justify-center items-center ">
+            <BsWhatsapp className="text-sm cursor-pointer"></BsWhatsapp>
+          </div>
+          <p className="text-stone-400 ml-2">+9 000000000</p>
+        </div>
+        <div className="flex justify-center items-center ">
+          <div className="w-[40px] h-[40px] rounded-full bg-blue-600 text-white flex justify-center items-center ">
+            <FaFacebookF className="text-sm cursor-pointer"></FaFacebookF>
+          </div>
+          <div className="w-[40px] h-[40px] rounded-full bg-blue-600 text-white flex justify-center items-center mx-2">
+            <FiInstagram className="text-sm cursor-pointer"></FiInstagram>
+          </div>
+          <div className="w-[40px] h-[40px] rounded-full bg-blue-600 text-white flex justify-center items-center ">
+            <FaLinkedinIn className="text-sm cursor-pointer"></FaLinkedinIn>
+          </div>
         </div>
       </div>
       <div className="text-center div-border py-8">
