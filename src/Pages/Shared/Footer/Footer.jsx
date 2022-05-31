96dfafbc894046d5c06f1b8e181cf119073914eb
NishatFariza <nishatfarzia78@gmail.com>
2022-05-31 23:13:36 +0600

    google fonts added

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -1,15 +1,35 @@
 import React from "react";
 import "./Footer.css";
+import { FaEnvelope } from "react-icons/fa";
 
 const Footer = () => {
+  const today = new Date();
+  const year = today.getFullYear();
   return (
-    <footer className="footer lg:h-[30vh]">
-      <div>
-        <p className="text-white">
-          Copyright 2011 <span>All Rights Reserve</span>{" "}
+    <div className="footer-bg ">
+      <div className="py-14 px-10 grid grid-cols-3 gap-5">
+        <div className="flex  justify-start items-center">
+          <FaEnvelope className="text-white mr-4"></FaEnvelope>
+          <p className="text-stone-400">info@gmail.com</p>
+          <p className="text-stone-400">info@gmail.com</p>
+        </div>
+        <div className="flex justify-center items-center ">
+          <FaEnvelope className="text-white mr-4"></FaEnvelope>
+          <p className="text-stone-400">info@gmail.com</p>
+          <p className="text-stone-400">info@gmail.com</p>
+        </div>
+        <div className="flex justify-center items-center ">
+          <FaEnvelope className="text-white"></FaEnvelope>
+          <FaEnvelope className="text-white mx-4"></FaEnvelope>
+          <FaEnvelope className="text-white"></FaEnvelope>
+        </div>
+      </div>
+      <div className="text-center div-border py-8">
+        <p className="text-center text-stone-400">
+          Copyright {year} <span>All Rights Reserve</span>
         </p>
       </div>
-    </footer>
+    </div>
   );
 };
 
