c9c204934823b47a139a20fc277902a1157cc3f3
NishatFariza <nishatfarzia78@gmail.com>
2022-06-01 09:59:20 +0600

    logo banner added

---
+++ src/Pages/Homepage/Banner/Banner.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Banner = () => {
+  return <div className="px-10">
+      
+  </div>;
+};
+
+export default Banner;
--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -48,6 +48,7 @@ const Header = () => {
       </div>
       <div>
           
+
       </div>
     </header>
   );
