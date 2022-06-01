88cfe535bd091da11a0d8acbb0ca68ba17e5b4ae
NishatFariza <nishatfarzia78@gmail.com>
2022-06-01 13:13:08 +0600

    sub header and main header added

--- src/Pages/Homepage/Banner/Banner.jsx
+++ src/Pages/Homepage/Banner/Banner.jsx
@@ -1,9 +1,85 @@
 import React from "react";
+import { FaHome } from "react-icons/fa";
+import "./Banner.css";
 
 const Banner = () => {
-  return <div className="px-10">
-      
-  </div>;
+  return (
+    <div className="bg-black">
+      <div className="lg:w-10/12 w-11/12  mx-auto flex  text-white">
+        <div className="lg:w-[8%] cursor-pointer border-l border-stone-700 text-center lg:py-4 md:py-3 py-2">
+          <FaHome className="mx-auto text-2xl"></FaHome>
+        </div>
+        <div className="lg:w-[16%] border-l border-stone-700 text-center lg:py-4 md:py-3 py-2">
+          Menu
+        </div>
+        <div className="lg:w-[16%] border-l border-stone-700 text-center lg:py-4 md:py-3 py-2">
+          Menu 1
+        </div>
+        <div className="lg:w-[10%] border-l border-stone-700 text-center lg:py-4 md:py-3 py-2">
+          Menu 2
+        </div>
+        <div className="lg:w-[8%] border-l border-stone-700 text-center lg:py-4 md:py-3 py-2"></div>
+        <div className="lg:w-[16%] border-l border-stone-700 text-center lg:py-4 md:py-3 py-2">
+         
+        </div>
+        <div className="lg:w-[13%] border-l border-stone-700 text-center lg:py-4 md:py-3 py-2">
+          
+        </div>
+        <div className="lg:w-[13%] border-x border-stone-700 text-center lg:py-4 md:py-3 py-2">
+          
+        </div>
+        {/* <div className="border-x py-6 border-yellow-800">
+        <FaHome></FaHome>
+      </div>
+      <div className="border-x py-6 border-yellow-800">
+        <FaHome></FaHome>
+      </div>
+      <div className="flex justify-around ">
+        <div>
+          <FaHome></FaHome>
+        </div>
+        <div className="border-x py-6 border-yellow-800">
+          <FaHome></FaHome>
+        </div>
+      </div>
+      <div className="border-x py-6 border-yellow-800">
+        <FaHome></FaHome>
+      </div>
+      <div className="flex justify-around ">
+        <div>
+          <FaHome></FaHome>
+        </div>
+        <div>
+          <FaHome></FaHome>
+        </div>
+      </div>
+      <div className="flex justify-around ">
+        <div>
+          <FaHome></FaHome>
+        </div>
+        <div>
+          <FaHome></FaHome>
+        </div>
+      </div>
+      <div className="flex justify-around ">
+        <div>
+          <FaHome></FaHome>
+        </div>
+        <div>
+          <FaHome></FaHome>
+        </div>
+      </div>
+      <div className="flex justify-around ">
+        <div>
+          <FaHome></FaHome>
+        </div>
+        <div>
+          <FaHome></FaHome>
+        </div>
+      </div> */}
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -12,42 +12,44 @@ const Footer = () => {
   const year = today.getFullYear();
   return (
     <div className="footer-bg ">
-      <div className="py-14 px-10 grid grid-cols-3 gap-5">
-        <div className="flex  justify-start items-center">
-          <div className="w-[40px] h-[40px] rounded-full bg-blue-600 text-white flex justify-center items-center ">
-            <FaEnvelope className="text-sm cursor-pointer"></FaEnvelope>
+      <div className="lg:w-10/12 w-11/12  mx-auto">
+        <div className="py-14 grid grid-cols-3 gap-5">
+          <div className="flex  justify-start items-center">
+            <div className="w-[40px] h-[40px] rounded-full bg-blue-600 text-white flex justify-center items-center ">
+              <FaEnvelope className="text-sm cursor-pointer"></FaEnvelope>
+            </div>
+            <p className="text-stone-400 ml-2">info@gmail.com</p>
+            <p className="text-stone-400 mx-2">/</p>
+            <p className="text-stone-400">info@gmail.com</p>
           </div>
-          <p className="text-stone-400 ml-2">info@gmail.com</p>
-          <p className="text-stone-400 mx-2">/</p>
-          <p className="text-stone-400">info@gmail.com</p>
+          <div className="flex justify-center items-center ">
+            <div className="w-[40px] h-[40px] rounded-full bg-blue-600 text-white flex justify-center items-center ">
+              <BsTelephoneFill className="text-sm cursor-pointer"></BsTelephoneFill>
+            </div>
+            <p className="text-stone-400  ml-2">+9 000000000</p>
+            <p className="text-stone-400 mx-2">/</p>
+            <div className="w-[40px] h-[40px] rounded-full bg-blue-600 text-white flex justify-center items-center ">
+              <BsWhatsapp className="text-sm cursor-pointer"></BsWhatsapp>
+            </div>
+            <p className="text-stone-400 ml-2">+9 000000000</p>
+          </div>
+          <div className="flex justify-center items-center ">
+            <div className="w-[40px] h-[40px] rounded-full bg-blue-600 text-white flex justify-center items-center ">
+              <FaFacebookF className="text-sm cursor-pointer"></FaFacebookF>
+            </div>
+            <div className="w-[40px] h-[40px] rounded-full bg-blue-600 text-white flex justify-center items-center mx-2">
+              <FiInstagram className="text-sm cursor-pointer"></FiInstagram>
+            </div>
+            <div className="w-[40px] h-[40px] rounded-full bg-blue-600 text-white flex justify-center items-center ">
+              <FaLinkedinIn className="text-sm cursor-pointer"></FaLinkedinIn>
+            </div>
+          </div>
         </div>
-        <div className="flex justify-center items-center ">
-          <div className="w-[40px] h-[40px] rounded-full bg-blue-600 text-white flex justify-center items-center ">
-            <BsTelephoneFill className="text-sm cursor-pointer"></BsTelephoneFill>
-          </div>
-          <p className="text-stone-400  ml-2">+9 000000000</p>
-          <p className="text-stone-400 mx-2">/</p>
-          <div className="w-[40px] h-[40px] rounded-full bg-blue-600 text-white flex justify-center items-center ">
-            <BsWhatsapp className="text-sm cursor-pointer"></BsWhatsapp>
-          </div>
-          <p className="text-stone-400 ml-2">+9 000000000</p>
+        <div className="text-center div-border py-8">
+          <p className="text-center text-stone-400">
+            Copyright {year} <span>All Rights Reserve</span>
+          </p>
         </div>
-        <div className="flex justify-center items-center ">
-          <div className="w-[40px] h-[40px] rounded-full bg-blue-600 text-white flex justify-center items-center ">
-            <FaFacebookF className="text-sm cursor-pointer"></FaFacebookF>
-          </div>
-          <div className="w-[40px] h-[40px] rounded-full bg-blue-600 text-white flex justify-center items-center mx-2">
-            <FiInstagram className="text-sm cursor-pointer"></FiInstagram>
-          </div>
-          <div className="w-[40px] h-[40px] rounded-full bg-blue-600 text-white flex justify-center items-center ">
-            <FaLinkedinIn className="text-sm cursor-pointer"></FaLinkedinIn>
-          </div>
-        </div>
-      </div>
-      <div className="text-center div-border py-8">
-        <p className="text-center text-stone-400">
-          Copyright {year} <span>All Rights Reserve</span>
-        </p>
       </div>
     </div>
   );
